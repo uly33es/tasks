fd811f5fa63b64b49494731ac8631200cc7d36d8
uly33es <0angelok2003@gmail.com>
2024-07-12 16:12:50 +0500

    Добавление функции, которая параметром будет принимать 3 числа

--- test.php
+++ test.php
@@ -2,4 +2,12 @@
 function summ($num1, $num2, $num3) {
     echo $num1 + $num2 + $num3;
 }
-summ(1,45,-20);
+//summ(1,45,-20);
+
+function summ1($num1, $num2, $num3) {
+    echo $num1 + $num2 + $num3;
+}
+$param1 = 1;
+$param2 = 2;
+$param3 = 3;
+//summ1($param1, $param2, $param3);
