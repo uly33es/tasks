3b833e65a9698a8ba550677a51782971259fb8da
uly33es <0angelok2003@gmail.com>
2024-08-01 17:08:38 +0500

    Создание второго объекта класса Employee

--- class.php
+++ class.php
@@ -5,8 +5,11 @@ class Employee
     public $age;
     public $salary;
 }
-$employee = new Employee();
-echo $employee->name = "john";
-echo $employee->age = 25;
-echo $employee->salary = 1000;
-
+$employee1 = new Employee();
+echo $employee1->name = "john";
+echo $employee1->age = 25;
+echo $employee1->salary = 1000;
+$employee2 = new Employee();
+echo $employee2->name = "eric";
+echo $employee2->age = 26;
+echo $employee2->salary = 2000;
