53523a78e225d31b89721e6f0c2622fc3481c405
uly33es <0angelok2003@gmail.com>
2024-08-01 16:28:27 +0500

    Описание класса Продукт и покупатель, вывод баланса

--- shopping.php
+++ shopping.php
@@ -1,5 +1,5 @@
 <?php
-class ShopProduct {
+/*class ShopProduct {
     public $title = "Стандартный товар";
     public $produserMainName = "Фамилия автора";
     public $producerFirstName = "Имя автора";
@@ -33,6 +33,90 @@ class ShopProduct {
 //    ."$product1->produserMainName\n";
 
 $product1 = new ShopProduct("Собачье сердце", "Михаил", "Булгаков", 500);
-print "Автор: {$product1->getProduser()}";
+print "Автор: {$product1->getProduser()}";*/
+
+
+/*
+ * Класс товар - Product
+ */
+class Product {
+    /*
+     * Объявление свойтсва
+     * Цена - $price
+     * Кол-во - $quantity
+     * Вес - $weight
+     * Наименование - $name
+     * Тип товара - $type
+     */
+    public $price;
+    public $quantity;
+    public $weight;
+    public $name;
+    public $type;
+
+    public function __construct($price, $quantity, $weight, $name, $type)
+    {
+        $this -> price = $price;
+        $this -> quantity = $quantity;
+        $this -> weight = $weight;
+        $this -> name = $name;
+        $this -> type = $type;
+    }
+    /*
+     *getInfo
+     */
+    public function getInfo() : array
+    {
+        return [
+            'name' => $this->name,
+            'price' => $this->price,
+            'type' => $this->type
+        ];
+    }
+
+}
+
+/*
+ * Класс Покупатель - Buyer
+ */
+class Buyer extends Product
+{
+    /*
+     * Объявление свойтсва
+     * Баланс - $balance
+     * Текущая корзина - $cart
+     * История купленных товаров - $story
+     */
+    private float $balance;
+    public array $cart = [];
+    private array $buyHistory = [];
+    /*
+     * Объявление метода
+     * Добавление товара в корзину - addingProduct
+     * Покупка товара из корзины - buyProduct
+     * Получение истории покупок - historyPurchase
+     * Получение баланс - getBalance
+     *
+     */
+    public function __construct($balance)
+    {
+        $this -> balance = $balance;
+    }
+
+    public function addingProduct ()
+    {
+        if ($this->quantity > 0){
+
+        }
+    }
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+}
+$Byer1 = new Buyer(10);
+//echo $Byer1->getBalance();
+$user = new Product(100,2,50, "яблоко", "фрукт");
+echo $user->getInfo();
 ?>
 
