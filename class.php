fb126564792f7695a8828da16e834bbefe6be2bd
uly33es <0angelok2003@gmail.com>
2024-08-01 17:11:47 +0500

    Вывод суммы зп созданных объектов

--- class.php
+++ class.php
@@ -6,10 +6,11 @@ class Employee
     public $salary;
 }
 $employee1 = new Employee();
-echo $employee1->name = "john";
-echo $employee1->age = 25;
-echo $employee1->salary = 1000;
+echo $employee1->name = "john \n";
+echo $employee1->age = 25,"\n";
+echo $employee1->salary = 1000,"\n";
 $employee2 = new Employee();
-echo $employee2->name = "eric";
-echo $employee2->age = 26;
-echo $employee2->salary = 2000;
+echo $employee2->name = "eric \n";
+echo $employee2->age = 26,"\n";
+echo $employee2->salary = 2000,"\n";
+echo $employee1->salary + $employee2->salary;
