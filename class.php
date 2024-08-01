7ff21329f26aa74b8a11f90f6715664c987006a8
uly33es <0angelok2003@gmail.com>
2024-08-01 17:24:26 +0500

    getName, getAge, getSalary,checkAge - создание методов

--- class.php
+++ class.php
@@ -4,13 +4,33 @@ class Employee
     public $name;
     public $age;
     public $salary;
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getAge()
+    {
+        return $this->age;
+    }
+    public function getSalary()
+    {
+        return $this->salary;
+    }
+    public function checkAge()
+    {
+        return ($this->age > 18)?"true":"false";
+    }
 }
-$employee1 = new Employee();
-echo $employee1->name = "john \n";
-echo $employee1->age = 25,"\n";
-echo $employee1->salary = 1000,"\n";
-$employee2 = new Employee();
-echo $employee2->name = "eric \n";
-echo $employee2->age = 26,"\n";
-echo $employee2->salary = 2000,"\n";
-echo $employee1->salary + $employee2->salary;
+$user1 = new Employee();
+$user1->name = "john";
+$user1->age = 25;
+$user1->salary = 1000;
+$user2 = new Employee();
+$user2->name = "eric";
+$user2->age = 26;
+$user2->salary = 2000;
+
+echo $user1->getName();
+echo $user1->getAge()."\n";
+echo $user1->getSalary() + $user2->getSalary()."\n";
+echo $user2->checkAge();
