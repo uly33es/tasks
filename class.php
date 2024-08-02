75455655a1ce9cfb1d3fb7c9622687274861114a
uly33es <0angelok2003@gmail.com>
2024-08-02 17:34:37 +0500

    Создание конструктора в классе Employee

--- class.php
+++ class.php
@@ -1,52 +1,69 @@
 <?php
-/*class Employee
+class Employee
 {
     public $name;
     public $age;
     public $salary;
-    public function getName()
-    {
-        return $this->name;
-    }
-    public function getAge()
-    {
-        return $this->age;
-    }
-    public function getSalary()
-    {
-        return $this->salary;
-    }
-    public function checkAge()
-    {
-        return ($this->age >= 18)?true:false;
-    }
-    public function setAge(int $age): void
-    {
-        if ($age >= 18) {
-            $this->age = $age;
+    /*    public function getName()
+        {
+            return $this->name;
+        }
+        public function getAge()
+        {
+            return $this->age;
+        }
+        public function getSalary()
+        {
+            return $this->salary;
+        }
+        public function checkAge()
+        {
+            return ($this->age >= 18)?true:false;
+        }
+        public function setAge(int $age): void
+        {
+            if ($age >= 18) {
+                $this->age = $age;
+            }
+        }
+        public function doubleSalary(){
+            return $this->salary * 2;
         }
     }
-    public function doubleSalary(){
-        return $this->salary * 2;
+    $user1 = new Employee();
+    $user1->name = "john";
+    $user1->age = 25;
+    $user1->salary = 1000;
+    $user2 = new Employee();
+    $user2->name = "eric";
+    $user2->age = 26;
+    $user2->salary = 2000;
+
+    echo $user1->getName();
+    echo $user1->getAge()."\n";
+    echo $user1->getSalary() + $user2->getSalary()."\n";
+    echo $user2->checkAge();
+    $user1->setAge(27);
+    echo $user1->doubleSalary()."\n";
+    echo $user1->age;*/
+
+    public function __construct($name, $age, $salary)
+    {
+        $this -> name = $name;
+        $this -> age = $age;
+        $this -> salary = $salary;
     }
+
 }
-$user1 = new Employee();
-$user1->name = "john";
-$user1->age = 25;
-$user1->salary = 1000;
-$user2 = new Employee();
-$user2->name = "eric";
-$user2->age = 26;
-$user2->salary = 2000;
-
-echo $user1->getName();
-echo $user1->getAge()."\n";
-echo $user1->getSalary() + $user2->getSalary()."\n";
-echo $user2->checkAge();
-$user1->setAge(27);
-echo $user1->doubleSalary()."\n";
-echo $user1->age;*/
-
+$user = new Employee("eric", 25, 1000);
+echo $user->name."\n";
+echo $user->age."\n";
+echo $user->salary."\n";
+$user2 = new Employee("kyle", 30, 2000);
+echo $user2->name."\n";
+echo $user2->age."\n";
+echo $user2->salary."\n";
+echo $user->salary + $user2->salary;
 /*class Rectangle
 {
    public $widht;
@@ -106,7 +123,7 @@ $user->setAge(30);
 echo $user->age."\n";
 $user->addAge(10);
 echo $user->age;*/
-
+/*
 class Student
 {
     public $name;
@@ -124,4 +141,4 @@ class Student
 }
 $student = new Student();
 $student->course = 2;
-echo $student->transferToNextCours(2);
+echo $student->transferToNextCours(2);*/
